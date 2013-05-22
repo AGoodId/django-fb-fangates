8f90e85d8e1d3daf929d0ce3f26b8ac3b7dc455d
Henrik Almer <henrik@agoodid.se>
2013-05-22 14:56:09 +0200

    Structured the admin

--- fb_fangates/admin.py
+++ fb_fangates/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
+from django.utils.translation import ugettext as _
+
+
+from begood_sites.admin import SiteModelAdmin
 from fb_fangates.models import FanGate
 
-class FanGateAdmin(admin.ModelAdmin):
-    pass
+
+class FanGateAdmin(SiteModelAdmin):
+  fieldsets = [
+    (None, {'fields': ['title', 'app_id', 'sites']}),
+    (_('For non-fans'), {'fields': ['non_fan_article', 'non_fan_content']}),
+    (_('For fans'), {'fields': ['show_fan_content', 'fan_article', 'fan_content']})
+  ]
+  search_fields = ['title', 'non_fan_content', 'fan_content']
+
 
 admin.site.register(FanGate, FanGateAdmin)
