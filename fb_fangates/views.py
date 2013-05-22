a7418754020a9ab01750282a967695c9327a4b89
Henrik Almer <henrik@agoodid.se>
2013-05-22 14:21:27 +0200

    Made fan gate function work by testing with a FB app

--- fb_fangates/views.py
+++ fb_fangates/views.py
@@ -14,6 +14,9 @@ class FanGateView(DetailView):
   context_object_name = 'fangate'
   template_name = 'fb/tab.html'
 
+  def post(self, *args, **kwargs):
+    return self.get(*args, **kwargs)
+
   def get_context_data(self, **kwargs):
     context = super(FanGateView, self).get_context_data(**kwargs)
 
@@ -24,17 +27,14 @@ class FanGateView(DetailView):
     # Parse data in the ``signed_request`` provided by Facebook
     fb_signed_request = self.request.POST.get('signed_request', None)
     if fb_signed_request:
-      try:
-        encoded_sig, payload = fb_signed_request.split('.')      
-        fb_user_data = json.loads(base64.b64decode(payload.replace('-_', '+/')))
-      except:
-        pass
-      else:
-        if fb_user_data['page']['liked']:
-          content = context['fangate'].fan_content
-          liked = True
+      encoded_sig, payload = fb_signed_request.split('.')
+      padded_payload = payload.replace('-_', '+/') + '=' * (4 - len(payload) % 4)
+      fb_user_data = json.loads(base64.b64decode(padded_payload))
+      if fb_user_data['page']['liked']:
+        content = context['fangate'].fan_content
+        liked = True
 
     # Update context and return
-    context.update({'content': content, 'liked': liked})
+    context.update(locals())
 
     return context
